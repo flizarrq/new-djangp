9fc977d2348b6fe63180bc885a768d02545782d1
byf1n <kirillozubenko@gmail.com>
2022-12-28 15:55:26 +0100

    test123

---
+++ bober/test123.py
@@ -0,0 +1,16 @@
+import platform
+import os
+
+# print(platform.uname())       # - system win / mak / lin
+# print(os.environ)  # - змiннi системи
+# os.system("C:\\Users\\kiril\\hot keys") # команды
+# print(os.getcwd()) # шлях до поточноi попки
+# print(os.listdir()) # що знаходиться на цьому шляху яки папки
+# os.mkdir('bob') # create folder
+# print(os.path.join('folder', '222', '1.txt')) # normalise path for all system
+
+# os.rename('test.py', os.path.join('bober', 'test123.py')) # rename file and change location
+
+# for root, folder, files in os.walk('C:\\Users'):
+#     print(f'{root=} {folder=} {files=}')
+
